9005a77c6d422a7185e04f481814da06ebbe104e
Artyom <artem_aleshin_2003@mail.ru>
2022-02-14 22:26:26 +0300

    task6

--- main.c
+++ main.c
@@ -1,40 +1,19 @@
 #include <stdio.h>
 #include "libs/data_structures/matrix/matrix.h"
 
-long long getSum(int *a, int size) {
-    int sum = 0;
-    for (size_t i = 0; i < size; ++i) {
-        sum += a[i];
-    }
-    return sum;
-}
-
-bool isUnique(long long *a, int size) {
-    for (size_t i = 1; i < size; ++i) {
-        if (a[i - 1] == a[i])
-            return false;
-    }
-    return true;
-}
-
-matrix task5(matrix m) {
-    int arrayOfSum[m.nRows];
-    for (size_t i = 0; i < m.nRows; ++i) {
-        arrayOfSum[i] = getSum(m.values[i], m.nCols);
-    }
-    if (isUnique(arrayOfSum, m.nRows))
-        transposeSquareMatrix(m);
-    return m;
-
+bool isMutuallyInverseMatrices(matrix m1, matrix m2) {
+    return isEMatrix(mulMatrices(m1, m2));
 }
 
 int main() {
-    matrix m = createMatrixFromArray((int[]) {1, 2, 3,
-                                              4, 5, 6,
-                                              7, 8, 9
-    }, 3, 3);
+    matrix m1 = createMatrixFromArray((int[]) {2, 5, 7,
+                                               6, 3, 4,
+                                               5, -2, -3}, 3, 3);
+    matrix m2 = createMatrixFromArray((int[]) {1, -1, 1,
+                                               -38, 41, -34,
+                                               27, -29, 24}, 3, 3);
 
-    m = task5(m);
-    outputMatrix(m);
+    printf("%d", isMutuallyInverseMatrices(m1, m2));
+
     return 0;
 }
