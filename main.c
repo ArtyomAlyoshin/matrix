357868156056fcbde40b89a0a1eb39bc2c2f5bd5
Artyom <artem_aleshin_2003@mail.ru>
2022-02-19 18:39:49 +0300

    task12

--- main.c
+++ main.c
@@ -1,33 +1,54 @@
 #include <stdio.h>
+#include <string.h>
 #include "libs/data_structures/matrix/matrix.h"
 
-int getNSpecialElement(matrix m) {
-    int NSpecial = 0;
-    for (int j = 0; j < m.nCols; j++) {
-        int specialElement = m.values[0][j];
-        int sumOfElementsInCol = 0;
-        for (int i = 1; i < m.nRows; i++) {
-            if (m.values[i][j] > specialElement) {
-                sumOfElementsInCol += specialElement;
-                specialElement = m.values[i][j];
-            } else
-                sumOfElementsInCol += m.values[i][j];
-        }
-        if (specialElement > sumOfElementsInCol)
-            NSpecial++;
-    }
-    return NSpecial;
+position getLeftMin(matrix m) {
+    int min = m.values[0][0];
+    position minPosition = {0, 0};
+
+    for (int j = 0; j < m.nCols; j++)
+        for (int i = 0; i < m.nRows; i++)
+            if (m.values[i][j] < min) {
+                min = m.values[i][j];
+                minPosition.colIndex = j;
+                minPosition.rowIndex = i;
+            }
+
+    return minPosition;
 }
 
+void swapPenultimateRow(matrix m) {
+    if (m.nRows < 2) {
+        printf("bad case");
+    }
+
+    int col[m.nRows];
+    position min = getLeftMin(m);
+
+    for (int i = 0; i < m.nRows; i++)
+        col[i] = m.values[i][min.colIndex];
+
+    memcpy(m.values[m.nRows - 2], col, sizeof(int) * m.nCols);
+}
 
 int main() {
     matrix m1 = createMatrixFromArray(
             (int[]) {
-                    3, 5, 5, 4,
-                    2, 3, 6, 7,
-                    12, 2, 1, 2
+                    1, 2, 3,
+                    4, 5, 6,
+                    7, 8, 1,
             },
-            3, 4);
+            3, 3);
 
-    assert(getNSpecialElement(m1) == 2);
+    swapPenultimateRow(m1);
+
+    matrix m2 = createMatrixFromArray(
+            (int[]) {
+                    1, 2, 3,
+                    1, 4, 7,
+                    7, 8, 1,
+            },
+            3, 3);
+
+    assert(twoMatricesEqual(m1, m2));
 }
