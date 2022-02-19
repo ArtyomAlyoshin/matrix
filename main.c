f7a2d5f044d13c0356474d0bee771a6bf90fb0c9
Artyom <artem_aleshin_2003@mail.ru>
2022-02-19 17:49:33 +0300

    task11

--- main.c
+++ main.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include "libs/data_structures/matrix/matrix.h"
 
-int countEqClassesByRowsSum(matrix m) {
-    long long rowSums[m.nRows];
-    for (int i = 0; i < m.nRows; i++)
-        rowSums[i] = getSum(m.values[i], m.nCols);
-
-    return countNUnique(rowSums, m.nRows);
+int getNSpecialElement(matrix m) {
+    int NSpecial = 0;
+    for (int j = 0; j < m.nCols; j++) {
+        int specialElement = m.values[0][j];
+        int sumOfElementsInCol = 0;
+        for (int i = 1; i < m.nRows; i++) {
+            if (m.values[i][j] > specialElement) {
+                sumOfElementsInCol += specialElement;
+                specialElement = m.values[i][j];
+            } else
+                sumOfElementsInCol += m.values[i][j];
+        }
+        if (specialElement > sumOfElementsInCol)
+            NSpecial++;
+    }
+    return NSpecial;
 }
 
 
 int main() {
     matrix m1 = createMatrixFromArray(
             (int[]) {
-                    7, 1,
-                    2, 7,
-                    5, 4,
-                    4, 3,
-                    1, 6,
-                    8, 0
+                    3, 5, 5, 4,
+                    2, 3, 6, 7,
+                    12, 2, 1, 2
             },
-            6, 2);
+            3, 4);
 
-    assert(countEqClassesByRowsSum(m1) == 3);
+    assert(getNSpecialElement(m1) == 2);
 }
